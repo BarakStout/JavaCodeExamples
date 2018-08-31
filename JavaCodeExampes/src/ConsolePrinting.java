f0d6ffe8c6e472f607698990c01fbd1ab2d6bbdf
Barak.Stout <Barak.Stout@MCL-FAC-8273L1Y.basisschools.local>
2018-08-31 16:39:04 -0400

    Initial Commit

---
+++ JavaCodeExampes/src/ConsolePrinting.java
@@ -0,0 +1,14 @@
+/**
+ * @author Barak.Stout
+ * 
+ * Printing to the console using System.out.println
+ * 
+ */
+
+public class ConsolePrinting 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");	
+	}	
+}
